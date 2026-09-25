e420c036394af3d48aca8baf32b8a7624c6679ff
3a7i3 <mathieuhasard111@gmail.com>
2026-09-24 23:44:44 -0700

    RL-REPLAY-01: expose immutable publication API

--- research_replay/__init__.py
+++ research_replay/__init__.py
@@ -9,13 +9,25 @@ from .factual import (
     replay_factual_dataset,
     validate_dataset,
 )
+from .publication import (
+    PublicationError,
+    PublicationResult,
+    RunExistsError,
+    build_publication_payload,
+    publish_factual_result,
+)
 
 __all__ = [
     "DatasetValidationError",
     "FactualReplayResult",
     "LifecycleRecord",
+    "PublicationError",
+    "PublicationResult",
     "ReplayError",
+    "RunExistsError",
     "ValidatedDataset",
+    "build_publication_payload",
+    "publish_factual_result",
     "replay_factual_dataset",
     "validate_dataset",
 ]
